5934fa4bf10615ea6f3228dfc251ef5010d23982
Abner Pena <abnrr.pena@gmail.com>
2022-09-28 17:59:00 -0700

    prueba de pull

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ABNER Y MARVIN
+          Prueba de pull
         </p>
         <a
           className="App-link"
